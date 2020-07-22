5d388d907abb47198a74ba81c13919e27ca53659
yuriyfomin17 <fominyura2012@gmail.com>
2020-07-22 18:03:40 +0100

    Updates

---
+++ precache-manifest.725086161b2dd375b2bf1d868ee95a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31f9610b9885b7ac016be8b26bda583",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "7cc5f24e8d6c0182ed4c",
+    "url": "/tic-tac-toe-game/static/css/main.278145d6.chunk.css"
+  },
+  {
+    "revision": "ddbd334d5c202bf27fed",
+    "url": "/tic-tac-toe-game/static/js/2.56675d98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-game/static/js/2.56675d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc5f24e8d6c0182ed4c",
+    "url": "/tic-tac-toe-game/static/js/main.1cf9ca73.chunk.js"
+  },
+  {
+    "revision": "27ef5eb149ff98f71fbd",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.4156cc54.js"
+  }
+]);
